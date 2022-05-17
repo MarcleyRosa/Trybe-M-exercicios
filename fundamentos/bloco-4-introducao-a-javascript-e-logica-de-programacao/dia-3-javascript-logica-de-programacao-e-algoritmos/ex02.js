ef3daf8b796caf4e955a11655d183533a2148dc6
Marcley <marcleyrosa@gmail.com>
2022-05-17 13:55:12 -0300

    Atualização ex02- dia3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex02.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex02.js
@@ -1,6 +1,6 @@
 let word = 'tryber';
 let wordInv = "Palavra Invertida: "
-for (let index = 0, n = 1; n <= word.length; index++, n++) {
+for (let index = 0, n = 1; n <= word.length; index++, n+= 1) {
     wordInv = wordInv + word[word.length - n]
 }
 
