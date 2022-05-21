86cffaf32a8ad47229443cce264f133e7758c910
Marcley <marcleyrosa@gmail.com>
2022-05-21 15:14:15 -0300

    Ex bloco 4 dia 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex06.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex06.js
@@ -13,3 +13,23 @@ let leitor = {
     ],
   };
    console.log('O livro favorito de',leitor.nome, leitor.sobrenome, "se chama", leitor['livrosFavoritos'][0].titulo);
+//Atividade 1
+   let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for (let ind in names) {
+    console.log('Olá', names[ind]);
+  }
+// Atividade 2
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let i in car) {
+    console.log(i, car[i]);
+  }
