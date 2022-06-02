9564ea1b68a4dec1efbb9672168a0067a2926433
Marcley <marcleyrosa@gmail.com>
2022-06-02 10:30:06 -0300

    Ex bloco 6 dia 2

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,7 @@
+// const eleButton = document.getElementById('buttonId');
+
+// function defaultButton(event) {
+//     event.preventDefault();
+// }
+
+// eleButton.addEventListener('click', defaultButton);
