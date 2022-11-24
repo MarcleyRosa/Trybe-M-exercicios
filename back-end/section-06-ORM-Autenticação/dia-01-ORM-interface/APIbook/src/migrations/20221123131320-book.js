0e05fbe1e2936887019ac1bad1531a60e24894ae
Marcley <marcleyrosa@gmail.com>
2022-11-23 21:14:41 -0300

    exercicios dia 01

---
+++ back-end/section-06-ORM-Autenticação/dia-01-ORM-interface/APIbook/src/migrations/20221123131320-book.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { Sequelize } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      pageQuantity: {
+        allowNull: true,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
