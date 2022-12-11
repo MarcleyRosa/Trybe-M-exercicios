78715e5bcaf0bad8d96962fbea272fa2725170c2
Marcley <marcleyrosa@gmail.com>
2022-12-11 10:58:09 -0300

    Config com error

--- back-end/section-08-TypeScript/dia-02-Tipagem/ex-course/dist/src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-exports.app = require('./app');
-const PORT = 3001;
-exports.app.listen(PORT, () => {
-    console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
-});
