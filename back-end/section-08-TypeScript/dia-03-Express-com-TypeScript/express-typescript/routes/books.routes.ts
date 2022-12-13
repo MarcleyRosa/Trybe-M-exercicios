9c06da173c3b759cec81c091a328004006a208bf
Marcley <marcleyrosa@gmail.com>
2022-12-13 20:38:06 -0300

    Configuração do sequelize do ex course

--- back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/express-typescript/routes/books.routes.ts
+++ back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/express-typescript/routes/books.routes.ts
@@ -1,5 +1,15 @@
 import { Router } from 'express';
+import BooksController from '../controllers/books.controller';
+// import validationBook from '../middlewares/books.middleware';
+const booksSlashId = '/books/:id';
 
 const router = Router();
+
+const booksController = new BooksController();
+router.get('/books', booksController.getAll);
+router.get(booksSlashId, booksController.getById);
+router.post('/books', booksController.create);
+router.put(booksSlashId, booksController.update);
+router.delete(booksSlashId, booksController.remove);
 
 export default router;
