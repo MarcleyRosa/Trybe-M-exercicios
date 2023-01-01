90d0f4f406f7c1830dffab0c60c52fb02c353edf
Marcley <marcleyrosa@gmail.com>
2023-01-01 20:07:14 -0300

    Ex 02

--- back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/typescript-express-ex/src/database/index.ts
+++ back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/typescript-express-ex/src/database/index.ts
@@ -1,5 +1,6 @@
 const express =  require('express');
 import userRoutes from './routes/userRouter';
+import postsRouter from './routes/postsRouter';
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -7,6 +8,8 @@ const { PORT = 3000 } = process.env;
 app.use(express.json());
 
 app.use('/users', userRoutes);
+
+app.use('/posts', postsRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening port ${PORT}`);
--- back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/typescript-express-ex/src/database/interfaces/index.ts
+++ back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/typescript-express-ex/src/database/interfaces/index.ts
@@ -10,3 +10,14 @@ export interface UserCredentials {
   export interface User extends IUser {
     id: number;
   }
+
+  export interface IPost {
+    title: string;
+    author: string;
+    category: string;
+    publicationDate: string;
+  }
+
+  export interface Post extends IPost {
+    id: number;
+  }
