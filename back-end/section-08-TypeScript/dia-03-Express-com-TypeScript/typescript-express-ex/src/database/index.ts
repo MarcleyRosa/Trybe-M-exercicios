ce6ee30dc2aacc85136deecb66556ca6283b1c7c
Marcley <marcleyrosa@gmail.com>
2023-01-02 09:27:25 -0300

    Ex 2 e 3

--- back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/typescript-express-ex/src/database/index.ts
+++ back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/typescript-express-ex/src/database/index.ts
@@ -1,6 +1,8 @@
 const express =  require('express');
 import userRoutes from './routes/userRouter';
 import postsRouter from './routes/postsRouter';
+import productsRouter from './routes/productsRouter';
+import restaurantRouter from './routes/restaurantRouter';
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -9,7 +11,11 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 
+app.use('/products', productsRouter);
+
 app.use('/posts', postsRouter);
+
+app.use('/restaurants', restaurantRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening port ${PORT}`);
--- back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/typescript-express-ex/src/database/interfaces/index.ts
+++ back-end/section-08-TypeScript/dia-03-Express-com-TypeScript/typescript-express-ex/src/database/interfaces/index.ts
@@ -21,3 +21,26 @@ export interface UserCredentials {
   export interface Post extends IPost {
     id: number;
   }
+
+  export interface IProduct {
+    name: string;
+    brand: string;
+    price: number;
+    manufacturingDate: string;
+    expirationDate: string;
+  }
+  
+  export interface Product extends IProduct {
+    id: number;
+  }
+
+  export interface IRestaurant {
+    name: string;
+    category: string;
+    openingTime: string;
+    closingTime: string;
+  }
+  
+  export interface Restaurant extends IRestaurant {
+    id: number;
+  }
